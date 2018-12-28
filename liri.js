7d6419feb6f105e2919f1e0ba03349d764daa4fe
dvld <nikoroberts620@gmail.com>
2018-12-28 14:32:36 -0600

    wrote liri js file, recieving errors on spotify search and concert search, movie search functioning

--- liri.js
+++ liri.js
@@ -1,38 +1,203 @@
+
+// dependencies
+// _____________________________________________
+
 // env file
 require('dotenv').config();
 
 // requires
-var fs = require('fs');
-var request = require('request');
-var keys = require('./keys.js');
-var spotify = require('node-spotify-api');
-var movieName = process.argv[3];
-var liriReturn = process.argv[2];
+var fs = require("fs");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var axios = require("axios");
 
-// switch/case commands
-switch (liriReturn) {
+// init spotify api client
+var spotify = new Spotify(keys.spotify);
 
-  case "concert-this":
-    concertThis();
-    break;
+// _________
 
-  case "spotify-this-song":
-    spotifyThisSong();
-    break;
+// functions
+// ______________________________________________
 
-  case "movie-this":
-    movieThis();
-    break;
+var getArtist = function (artist) {
 
-  case "do-what-it-says":
-    doWhatItSays();
-    break;
+  return artist.name;
+
 };
 
-// create function to call Bands in Town "concert this"
+// _________
 
-// create function to call spotify "spotify this song"
+var getSpotify = function (songName) {
 
-// create function to call omdb "movie this"
+  if (songName === undefined) {
 
-// create function "do what it says"
+    songName = "The Sign";
+
+  }
+
+  spotify.search(
+
+    {
+      type: "track",
+      query: songName
+    },
+
+    function (error, data) {
+
+      if (error) {
+
+        console.log("Error occured: " + error);
+
+        return;
+
+      }
+
+      var songs = data.tracks.items;
+
+      for (var i = 0; i < songs.length; i++) {
+
+        console.log(i);
+        console.log("Artist(s): " + songs[i].artist.map(getArtist));
+        console.log("Song name: " + songs[i].name);
+        console.log("Preview song: " + songs[i].preview_url);
+        console.log("Album: " + songs[i].album.name);
+
+      }
+
+    }
+  );
+};
+
+// _________
+
+var getBands = function (artist) {
+
+  var queryURL = "https:rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(queryURL).then(
+
+    function (response) {
+
+      var jsonData = response.data;
+
+      if (!jsonData.length) {
+
+        console.log("No results found for " + artist);
+
+        return;
+
+      }
+
+      console.log("Upcoming concerts for " + artist + ":");
+
+      for (var i = 0; i < jsonData.length; i++) {
+
+        var show = jsonData[i];
+
+        console.log(
+          show.venue.city + "," +
+          (show.venue.region || show.venue.country) +
+          " at " +
+          show.venue.name +
+          " " +
+          moment(show.datetime).format("MM/DD/YYYY")
+        );
+      }
+    }
+  );
+};
+
+// _________
+
+var getMovie = function (movieName) {
+
+  if (movieName === undefined) {
+
+    movieName = "Mr Nobody";
+
+  }
+
+  var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  axios.get(url).then(
+
+    function (response) {
+
+      var jsonData = response.data;
+
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+
+    }
+  );
+};
+
+// _________
+
+var doWhatItSays = function () {
+
+  fs.readFile("random.txt", "utf8", function (error, data) {
+
+    console.log(data);
+
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+
+      choose(dataArr[0], dataArr[1]);
+
+    } else if (dataArr.length === 1) {
+
+      choose(dataArr[0]);
+
+    }
+  });
+};
+
+// _________
+
+var choose = function (caseData, functionData) {
+
+  switch (caseData) {
+
+    case "spotify-this-song":
+      getSpotify(functionData);
+      break;
+
+    case "concert-this":
+      getBands(functionData);
+      break;
+
+    case "movie-this":
+      getMovie(functionData);
+      break;
+
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+
+    default:
+      console.log("LIRI doesn't know that");
+
+  }
+};
+
+// _________
+
+var run = function (argOne, argTwo) {
+
+  choose(argOne, argTwo);
+
+};
+
+// ___________________________________________________
+
+run(process.argv[2], process.argv.slice(3).join(" "));
